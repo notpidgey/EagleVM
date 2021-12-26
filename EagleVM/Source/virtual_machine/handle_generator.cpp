da7cacb5709ff281687c2b4d050299161231e264
Pidgey <notpidgey@users.noreply.github.com>
2021-12-26 16:18:55 -0500

    🐛 Fixed typo in vm_exit to pop arguments

--- EagleVM/Source/virtual_machine/handle_generator.cpp
+++ EagleVM/Source/virtual_machine/handle_generator.cpp
@@ -32,17 +32,17 @@ handle_info vm_handle_generator::create_vm_exit()
 
     //pushfq
     {
-        std::vector<uint8_t> encoded_instruction = zydis_helper::encode_instruction_0(ZYDIS_MNEMONIC_PUSHFQ);
+        std::vector<uint8_t> encoded_instruction = zydis_helper::encode_instruction_0(ZYDIS_MNEMONIC_POPFQ);
         vm_exit_operations.push_back(encoded_instruction);
     }
 
-    //push r0-r15 to stack
+    //pop r0-r15 to stack
     for (int i = ZYDIS_REGISTER_R15; i >= ZYDIS_REGISTER_RAX; i--)
     {
         ZydisReg reg = zydis_helper::create_new_operand<ZydisReg>();
         reg.value = static_cast<ZydisRegister>(i);
 
-        std::vector<uint8_t> encoded_instruction = zydis_helper::encode_instruction_1(ZYDIS_MNEMONIC_PUSH, reg);
+        std::vector<uint8_t> encoded_instruction = zydis_helper::encode_instruction_1(ZYDIS_MNEMONIC_POP, reg);
         vm_exit_operations.push_back(encoded_instruction);
     }
 
