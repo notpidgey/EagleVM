de147d86bddfce78cf7d0e0cdb902be8d16cb5a1
Pidgey <lilpidgey@users.noreply.github.com>
2021-12-28 19:52:57 -0500

    ✨ Updated PE generator to take arguments from previous PE

--- EagleVM/EagleVM.cpp
+++ EagleVM/EagleVM.cpp
@@ -6,49 +6,53 @@
 
 int main(int argc, char* argv[])
 {
-    pe_parser parser = pe_parser("D:\\VM\\ovtest.exe");
-    std::printf("[+] loaded ovtest.exe -> %i bytes\n", parser.get_file_size());
+	pe_parser parser = pe_parser("D:\\VM\\ovtest.exe");
+	std::printf("[+] loaded ovtest.exe -> %i bytes\n", parser.get_file_size());
 
-    int i = 1;
+	int i = 1;
 
-    auto sections = parser.get_sections();
-    std::printf("[>] parsed %i sections\n", parser.get_sections().size());
-    std::printf("%2s %-10s %-10s %-10s\n", "", "name", "va", "size");
-    std::for_each(sections.begin(), sections.end(), [&i](PIMAGE_SECTION_HEADER image_section)
-    {
-        std::printf("%2i %-10s %-10d %-10d\n", i, image_section->Name, image_section->VirtualAddress, image_section->SizeOfRawData);
-        i++;
-    });
+	auto sections = parser.get_sections();
+	std::printf("[>] parsed %i sections\n", parser.get_sections().size());
+	std::printf("%2s %-10s %-10s %-10s\n", "", "name", "va", "size");
+	std::for_each(sections.begin(), sections.end(), [&i](PIMAGE_SECTION_HEADER image_section)
+		{
+			std::printf("%2i %-10s %-10d %-10d\n", i, image_section->Name, image_section->VirtualAddress, image_section->SizeOfRawData);
+			i++;
+		});
 
 
-    i = 1;
-    auto imports = parser.get_dll_imports();
-    std::printf("\n[>] %i imports\n", imports.size());
-    std::printf("%3s %-20s %-s\n", "", "source", "import");
-    std::for_each(imports.begin(), imports.end(), [&i](std::pair<std::string, std::string>& dll_import)
-    {
-        std::printf("%3i %-20s %-s\n", i, dll_import.first.c_str(), dll_import.second.c_str());
-        i++;
-    });
+	i = 1;
+	auto imports = parser.get_dll_imports();
+	std::printf("\n[>] %i imports\n", imports.size());
+	std::printf("%3s %-20s %-s\n", "", "source", "import");
+	std::for_each(imports.begin(), imports.end(), [&i](std::pair<std::string, std::string>& dll_import)
+		{
+			std::printf("%3i %-20s %-s\n", i, dll_import.first.c_str(), dll_import.second.c_str());
+			i++;
+		});
 
-    auto nt_header = parser.get_nt_header();
-    std::printf("\n[>] stack reserve -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfStackReserve);
-    std::printf("[>] stack commit -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfStackCommit);
-    std::printf("[>] heap reserve -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfHeapReserve);
-    std::printf("[>] heap commit -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfHeapCommit);
+	auto nt_header = parser.get_nt_header();
+	std::printf("\n[>] stack reserve -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfStackReserve);
+	std::printf("[>] stack commit -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfStackCommit);
+	std::printf("[>] heap reserve -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfHeapReserve);
+	std::printf("[>] heap commit -> %I64d bytes\n", nt_header->OptionalHeader.SizeOfHeapCommit);
 
-    pe_generator generator;
-    PIMAGE_DOS_HEADER dos_header = generator.build_dos_header();
-    PIMAGE_FILE_HEADER file_header = generator.build_coff_header();
-    PIMAGE_OPTIONAL_HEADER optional_header = generator.build_optional_header(
-        nt_header->OptionalHeader.SizeOfStackReserve,
-        nt_header->OptionalHeader.SizeOfStackCommit,
-        nt_header->OptionalHeader.SizeOfHeapReserve,
-        nt_header->OptionalHeader.SizeOfHeapCommit
-    );
-    
-    //vm_handle_generator::create_vm_enter();
-    //vm_handle_generator::create_vm_exit();
+	pe_generator generator;
+	PIMAGE_DOS_HEADER dos_header = generator.build_dos_header();
+	PIMAGE_FILE_HEADER file_header = generator.build_coff_header();
+	PIMAGE_OPTIONAL_HEADER optional_header = generator.build_optional_header(
+		nt_header->OptionalHeader.ImageBase,
+		nt_header->OptionalHeader.SectionAlignment,
+		nt_header->OptionalHeader.FileAlignment,
+		nt_header->OptionalHeader.SizeOfStackReserve,
+		nt_header->OptionalHeader.SizeOfStackCommit,
+		nt_header->OptionalHeader.SizeOfHeapReserve,
+		nt_header->OptionalHeader.SizeOfHeapCommit
+	);
+	auto sections = generator.build_section_table(sections);
 
-    return 0;
+	//vm_handle_generator::create_vm_enter();
+	//vm_handle_generator::create_vm_exit();
+
+	return 0;
 }
--- EagleVM/Headers/pe/pe_generator.h
+++ EagleVM/Headers/pe/pe_generator.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <algorithm>
 #include <Windows.h>
 
 #include "pe/pe_sections/pe_code_section.h"
@@ -16,12 +17,14 @@ class pe_generator
 public:
 	PIMAGE_DOS_HEADER build_dos_header();
 	PIMAGE_FILE_HEADER build_coff_header();
-	PIMAGE_OPTIONAL_HEADER build_optional_header(uint64_t sr, uint64_t sc, uint64_t hr, uint64_t hc);
-	std::vector<char>& build_section_table();
+	PIMAGE_OPTIONAL_HEADER build_optional_header(uint64_t image_base, int section_alignment, int file_alignment, uint64_t sr, uint64_t sc, uint64_t hr, uint64_t hc);
+	std::vector<IMAGE_SECTION_HEADER>* build_section_table(std::vector<PIMAGE_SECTION_HEADER> sections);
 
 private:
-	IMAGE_DOS_HEADER dos_header;
-	IMAGE_FILE_HEADER coff_header;
-	IMAGE_OPTIONAL_HEADER optional_header;
-	std::vector<IMAGE_SECTION_HEADER> section_headers;
+	IMAGE_SECTION_HEADER vm_handler_section;
+
+	IMAGE_DOS_HEADER dos_header{};
+	IMAGE_FILE_HEADER coff_header{};
+	IMAGE_OPTIONAL_HEADER optional_header{};
+	std::vector<IMAGE_SECTION_HEADER> section_headers{};
 };
