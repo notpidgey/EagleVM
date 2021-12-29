0af8945140cdf2d7da09fce250c4b25fcfc5c348
Pidgey <lilpidgey@users.noreply.github.com>
2021-12-28 23:43:08 -0500

    🎉 Created stub DLL project

--- EagleVMSandbox/EagleVMSandbox.cpp
+++ EagleVMSandbox/EagleVMSandbox.cpp
@@ -1,7 +1,19 @@
-#include "vm_context.h"
+#include "EagleVMStub.h"
+#include <cstdio>
 
 int main(int argc, char* argv[])
 {
-    
+    fnEagleVMBegin();
+
+    int i = 5;
+    i++;
+    i--;
+
+    int x = 0;
+    x += i;
+
+    printf("test %i %i", i, x);
+
+    fnEagleVMEnd();
     return 0;
 }
---
+++ EagleVMStub/EagleVMStub.cpp
@@ -0,0 +1,7 @@
+#define EAGLEVMSTUB_EXPORTS
+
+#include "pch.h"
+#include "EagleVMStub.h"
+
+EAGLEVMSTUB_API void __stdcall fnEagleVMBegin(void) { }
+EAGLEVMSTUB_API void __stdcall fnEagleVMEnd(void) { }
