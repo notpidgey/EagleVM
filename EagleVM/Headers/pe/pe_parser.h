b62d7823c1af689662f0fd1090ab2e25afa03c56
Pidgey <notpidgey@users.noreply.github.com>
2021-12-31 14:29:13 -0500

    🎨 Moved logging out of parsing functions

--- EagleVM/Headers/pe/pe_parser.h
+++ EagleVM/Headers/pe/pe_parser.h
@@ -12,6 +12,13 @@ struct pe_import
 	std::string import_name;
 };
 
+enum class stub_import
+{
+	unknown = 0,
+	vm_begin,
+	vm_end,
+};
+
 class pe_parser
 {
 public:
@@ -22,23 +29,22 @@ public:
 	bool read_file(const char* path);
 	int get_file_size();
 
-	std::vector<std::pair<uint32_t, uint32_t>> find_iat_calls();
+	std::vector<std::pair<uint32_t, stub_import>> find_iat_calls();
 
 	PIMAGE_DOS_HEADER get_dos_header();
 	PIMAGE_NT_HEADERS get_nt_header();
+	PIMAGE_SECTION_HEADER get_import_section();
+	IMAGE_DATA_DIRECTORY get_directory(short directory);
 	
 	std::vector<PIMAGE_SECTION_HEADER> get_sections();
 	PIMAGE_SECTION_HEADER get_section(const std::string& section_name);
 	PIMAGE_SECTION_HEADER get_section_rva(const uint32_t rva);
 	PIMAGE_SECTION_HEADER get_section_offset(const uint32_t offset);
-	PIMAGE_SECTION_HEADER get_import_section();
 
-	IMAGE_DATA_DIRECTORY get_directory(short directory);
-
-	void enum_imports(std::function<void(PIMAGE_IMPORT_DESCRIPTOR, PIMAGE_THUNK_DATA, PIMAGE_SECTION_HEADER, int index, char*)> import_proc);
-private:
-	std::vector<char> unprotected_pe_;
+	void enum_imports(std::function<void(PIMAGE_IMPORT_DESCRIPTOR, PIMAGE_THUNK_DATA, PIMAGE_SECTION_HEADER, int, char*)> import_proc);
 
 	uint32_t offset_to_rva(uint32_t offset);
 	uint32_t rva_to_offset(uint32_t rva);
+private:
+	std::vector<char> unprotected_pe_;
 };
