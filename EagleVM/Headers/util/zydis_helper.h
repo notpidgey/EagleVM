d444cd66e316bda91145c6323125137e28ba0b4b
Pidgey <lilpidgey@users.noreply.github.com>
2022-01-02 18:23:05 -0500

    ⚡️ Huge brain template request encoder

--- EagleVM/Headers/util/zydis_helper.h
+++ EagleVM/Headers/util/zydis_helper.h
@@ -33,45 +33,18 @@ namespace zydis_helper
 	std::vector<uint8_t> encode_request(ZydisEncoderRequest& request);
 	ZydisEncoderRequest create_encode_request(ZydisMnemonic mnemonic);
 
-	void add_imm(ZydisEncoderRequest& req, ZydisImm imm);
-	void add_mem(ZydisEncoderRequest& req, ZydisMem mem);
-	void add_ptr(ZydisEncoderRequest& req, ZydisPtr ptr);
-	void add_reg(ZydisEncoderRequest& req, ZydisReg reg);
+	void add_op(ZydisEncoderRequest& req, ZydisImm imm);
+	void add_op(ZydisEncoderRequest& req, ZydisMem mem);
+	void add_op(ZydisEncoderRequest& req, ZydisPtr ptr);
+	void add_op(ZydisEncoderRequest& req, ZydisReg reg);
 
 	std::vector<uint8_t> encode_queue(std::vector<ZydisEncoderRequest>& queue);
 
-	template<typename T>
-	void add_operand(ZydisEncoderRequest& encoder, T* operand)
+	template<ZydisMnemonic TMnemonic, typename... TArgs>
+	ZydisEncoderRequest create_encode_request(TArgs&&... args)
 	{
-		if (std::is_same<T, ZydisImm>::value)
-			add_imm(encoder, *(ZydisImm*)operand);
-		else if (std::is_same<T, ZydisMem>::value)
-			add_mem(encoder, *(ZydisMem*)operand);
-		else if (std::is_same<T, ZydisPtr>::value)
-			add_ptr(encoder, *(ZydisPtr*)operand);
-		else if (std::is_same<T, ZydisReg>::value)
-			add_reg(encoder, *(ZydisReg*)operand);
-	}
-
-	template<typename T>
-	ZydisEncoderRequest create_encode_request(ZydisMnemonic mnemonic, T operand1)
-	{
-		auto encoder = zydis_helper::create_encode_request(mnemonic);
-
-		//I feel terrible for having to write this, but it had to be done
-		add_operand<T>(encoder, &operand1);
-
-		return encoder;
-	}
-
-	template<typename T, typename G>
-	ZydisEncoderRequest create_encode_request(ZydisMnemonic mnemonic, T operand1, G operand2)
-	{
-		auto encoder = zydis_helper::create_encode_request(mnemonic);
-
-		//I feel terrible for having to write this, but it had to be done
-		add_operand<T>(encoder, &operand1);
-		add_operand<G>(encoder, &operand2);
+		auto encoder = zydis_helper::create_encode_request(TMnemonic);
+		(add_op(encoder, std::forward<TArgs>(args)), ...);
 
 		return encoder;
 	}
