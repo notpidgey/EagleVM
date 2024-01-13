1ed7017c6ef4964e5232620f1c552168535c77ef
Pidgey <notpidgey@users.noreply.github.com>
2024-01-13 14:57:20 -0500

    added mba math file

---
+++ EagleVM/Source/obfuscation/mba/math/mba_math.cpp
@@ -0,0 +1,11 @@
+#include "obfuscation/mba/math/mba_math.h"
+
+std::shared_ptr<mba_var_exp> mba_math::create_invertible_poly()
+{
+    return std::shared_ptr<mba_var_exp>();
+}
+
+std::shared_ptr<mba_var_exp> mba_math::create_invertible_linear()
+{
+    return std::shared_ptr<mba_var_exp>();
+}
