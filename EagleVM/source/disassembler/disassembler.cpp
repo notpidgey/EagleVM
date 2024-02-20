2fa2ab3419cf2dcd44554ae205831621df916f53
Pidgey <notpidgey@users.noreply.github.com>
2024-02-20 04:58:52 -0500

    fixed?

--- EagleVM/source/disassembler/disassembler.cpp
+++ EagleVM/source/disassembler/disassembler.cpp
@@ -32,7 +32,7 @@ void segment_disassembler::generate_blocks()
             const auto immediate = op.imm;
 
             // conditional jumps can either go somewhere else, or next
-            block->target_rvas.push_back(immediate.value.u);
+            block->target_rvas.push_back(current_rva + immediate.value.u + 2);
             block->target_rvas.push_back(block->end_rva_inc);
 
             blocks.push_back(block);
@@ -40,13 +40,72 @@ void segment_disassembler::generate_blocks()
 
             block_start_rva = block->end_rva_inc;
         }
-        else
-        {
-            block_instructions.push_back(inst);
-        }
 
         current_rva += inst.instruction.length;
     }
+
+    if(!block_instructions.empty())
+    {
+        zydis_decode& inst = function.back();
+
+        basic_block* block = new basic_block();
+        block->start_rva = block_start_rva;
+        block->end_rva = current_rva;
+        block->end_rva_inc = current_rva + inst.instruction.length;
+        block->instructions = block_instructions;
+
+        const zydis_decoded_operand op = inst.operands[0];
+        const auto immediate = op.imm;
+
+        block->target_rvas.push_back(current_rva + immediate.value.u + 2);
+        block->target_rvas.push_back(block->end_rva_inc);
+
+        blocks.push_back(block);
+    }
+
+    // but here we have an issue, some block may be jumping inside of another block
+    // we need to fix that
+    std::vector<basic_block*> new_blocks;
+
+    for(auto& block : blocks)
+    {
+        const uint32_t far_rva = block->target_rvas.back();
+        for(const auto& target_block : blocks)
+        {
+            if(far_rva > target_block->start_rva && far_rva < target_block->end_rva_inc)
+            {
+                // we found a jump to the middle of a block
+                // we need to split the block
+                basic_block* new_block = new basic_block();
+                new_block->start_rva = far_rva;
+                new_block->end_rva = target_block->end_rva;
+                new_block->end_rva_inc = target_block->end_rva_inc;
+
+                // for the new_block, we copy all the instructions starting at the far_rva
+                uint32_t current_rva = target_block->start_rva;
+                for(int i = 0; i < target_block->instructions.size(); i++)
+                {
+                    zydis_decode& inst = target_block->instructions[i];
+                    if(current_rva >= far_rva)
+                    {
+                        new_block->instructions.push_back(inst);
+
+                        // also remove the instruction from the old block
+                        target_block->instructions.erase(target_block->instructions.begin() + i);
+                    }
+
+                    current_rva += inst.instruction.length;
+                }
+
+                target_block->end_rva_inc = far_rva;
+                target_block->end_rva = far_rva;
+
+                new_blocks.push_back(new_block);
+            }
+        }
+    }
+
+    blocks.insert(blocks.end(), new_blocks.begin(), new_blocks.end());
 
     root_block = blocks[0];
     for(auto& block : blocks)
