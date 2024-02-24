1822496051543fceaec4237e8cedcc51c21edc4d
Pidgey <notpidgey@users.noreply.github.com>
2024-02-24 00:15:04 -0500

    fixed mov instruction to reflect intended behavior

--- EagleVM/source/virtual_machine/handlers/ia32_handlers/mov.cpp
+++ EagleVM/source/virtual_machine/handlers/ia32_handlers/mov.cpp
@@ -8,6 +8,7 @@ void ia32_mov_handler::construct_single(function_container& container, reg_size 
     // the address we want to move TO should be located right below it
 
     const inst_handler_entry* pop_handler = hg_->inst_handlers[ZYDIS_MNEMONIC_POP];
+    auto target_temp = zydis_helper::get_bit_version(VTEMP, size);
 
     // pop                  ; load the value into VTEMP
     // mov VTEMP2, [VSP]    ; now the address at VSP is the address we want to write to
@@ -15,7 +16,7 @@ void ia32_mov_handler::construct_single(function_container& container, reg_size 
     // pop                  ; we want to maintain the stack
     call_vm_handler(container, pop_handler->get_handler_va(size, 1));
     container.add(zydis_helper::enc(ZYDIS_MNEMONIC_MOV, ZREG(VTEMP2), ZMEMBD(VSP, 0, 8)));
-    container.add(zydis_helper::enc(ZYDIS_MNEMONIC_MOV, ZMEMBD(VTEMP2, 0, 8), ZREG(VTEMP)));
+    container.add(zydis_helper::enc(ZYDIS_MNEMONIC_MOV, ZMEMBD(VTEMP2, 0, size), ZREG(target_temp)));
     call_vm_handler(container, pop_handler->get_handler_va(bit64, 1));
 
     create_vm_return(container);
