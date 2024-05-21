286c2ede81ab00f9ea00cab8e1be2628a7dc64f0
Pidgey <notpidgey@users.noreply.github.com>
2024-05-21 15:32:14 -0400

    reworked shuffle regs function

--- EagleVM.Core/source/virtual_machine/machines/pidgeon/inst_regs.cpp
+++ EagleVM.Core/source/virtual_machine/machines/pidgeon/inst_regs.cpp
@@ -3,55 +3,59 @@
 #include "eaglevm-core/util/random.h"
 #include "eaglevm-core/codec/zydis_helper.h"
 
-#include "eaglevm-core/virtual_machine/models/vm_defs.h"
-
 namespace eagle::virt::pidg
 {
-    inst_regs::inst_regs()
+    inst_regs::inst_regs(const uint8_t temp_count)
     {
-        stack_order = {};
-        vm_order = {};
+        stack_order = { };
+        vm_order = { };
+
+        number_of_vregs = 8;
+        temp_variables = temp_count;
     }
 
     void inst_regs::init_reg_order()
     {
-        // todo: find a safer way to do this instead of enumearting enum
-        for(int i = ZYDIS_REGISTER_RAX; i <= ZYDIS_REGISTER_R15; i++)
-            stack_order[i - ZYDIS_REGISTER_RAX] = static_cast<codec::zydis_register>(i);
-
-        std::ranges::shuffle(stack_order, util::ran_device::get().gen);
-
-        bool success_shuffle = false;
-        std::array<codec::zydis_register, 16> access_order = stack_order;
-
-        while(!success_shuffle)
+        // setup the scratch register stack order
         {
-            std::ranges::shuffle(access_order, util::ran_device::get().gen);
+            // todo: find a safer way to do this instead of enumearting enum
+            for (int i = ZYDIS_REGISTER_RAX; i <= ZYDIS_REGISTER_R15; i++)
+                stack_order[i - ZYDIS_REGISTER_RAX] = static_cast<codec::zydis_register>(i);
 
-            success_shuffle = true;
-            for(int i = 0; i < NUM_OF_VREGS; i++)
-            {
-                const codec::zydis_register target = access_order[i];
-
-                // these are registers we do not want to be using as VM registers
-                if(target == ZYDIS_REGISTER_RIP || target == ZYDIS_REGISTER_RAX || target == ZYDIS_REGISTER_RSP)
-                {
-                    success_shuffle = false;
-                    break;
-                }
-            }
+            // shuffle the scratch registers
+            std::ranges::shuffle(stack_order, util::ran_device::get().gen);
         }
 
-        vm_order = access_order;
+        // completely separate | setup virtual machine registers
+        {
+            std::vector<codec::zydis_register> vm_regs_order;
+            vm_regs_order.append_range(stack_order);
+
+            std::erase(vm_regs_order, ZYDIS_REGISTER_RAX);
+            std::erase(vm_regs_order, ZYDIS_REGISTER_RSP);
+
+            std::ranges::shuffle(vm_regs_order, util::ran_device::get().gen);
+
+            // Add the undesirable registers back to the end of the array
+            vm_regs_order.push_back(ZYDIS_REGISTER_RAX);
+            vm_regs_order.push_back(ZYDIS_REGISTER_RSP);
+
+            std::copy(vm_regs_order.begin(), vm_regs_order.end(), vm_order);
+        }
     }
 
     codec::zydis_register inst_regs::get_reg(const uint8_t target) const
     {
         // this would be something like VIP, VSP, VTEMP, etc
-        if(target > NUM_OF_VREGS - 1 )
-            __debugbreak();
-
+        assert(target <= number_of_vregs - 1, "attempt to access invalid vreg");
         return vm_order[target];
+    }
+
+    codec::zydis_register inst_regs::get_reg_temp(const uint8_t target) const
+    {
+        // this would be something like VTEMP, VTEMP2, VTEMP3
+        assert(target <= temp_variables - 1, "attemp to access invalid vtemp");
+        return vm_order[temp_variables + target];
     }
 
     std::pair<uint32_t, codec::reg_size> inst_regs::get_stack_displacement(const codec::reg reg) const
@@ -63,9 +67,9 @@ namespace eagle::virt::pidg
         int found_index = 0;
         constexpr auto reg_count = stack_order.size();
 
-        for(int i = 0; i < reg_count; i++)
+        for (int i = 0; i < reg_count; i++)
         {
-            if(bit64_reg == stack_order[i])
+            if (bit64_reg == stack_order[i])
             {
                 found_index = i;
                 break;
@@ -73,9 +77,9 @@ namespace eagle::virt::pidg
         }
 
         int offset = 0;
-        if(reg_size == codec::reg_size::bit_8)
+        if (reg_size == codec::reg_size::bit_8)
         {
-            if(is_upper_8(reg))
+            if (is_upper_8(reg))
                 offset = 1;
         }
 
@@ -84,7 +88,7 @@ namespace eagle::virt::pidg
 
     void inst_regs::enumerate(const std::function<void(codec::zydis_register)>& enumerable, const bool from_back)
     {
-        if(from_back)
+        if (from_back)
             std::ranges::for_each(stack_order, enumerable);
         else
             std::for_each(stack_order.rbegin(), stack_order.rend(), enumerable);
