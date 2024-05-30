9b02d7c3667926fd2b441ec7bdac18447bb79bd9
Pidgey <notpidgey@users.noreply.github.com>
2024-05-30 03:36:03 -0400

    fixed dec handler names

--- EagleVM.Core/source/virtual_machine/ir/x86/handlers/dec.cpp
+++ EagleVM.Core/source/virtual_machine/ir/x86/handlers/dec.cpp
@@ -10,17 +10,17 @@ namespace eagle::ir::handler
         // todo: make vector of supported signatures
         // todo: make vector of handlers to generate
         valid_operands = {
-            { { { codec::op_none, codec::bit_8 } }, "8" },
-            { { { codec::op_none, codec::bit_16 } }, "16" },
-            { { { codec::op_none, codec::bit_32 } }, "32" },
-            { { { codec::op_none, codec::bit_64 } }, "64" },
+            { { { codec::op_none, codec::bit_8 } }, "dec 8" },
+            { { { codec::op_none, codec::bit_16 } }, "dec 16" },
+            { { { codec::op_none, codec::bit_32 } }, "dec 32" },
+            { { { codec::op_none, codec::bit_64 } }, "dec 64" },
         };
 
         build_options = {
-            { { ir_size::bit_8 }, "8" },
-            { { ir_size::bit_16 }, "16" },
-            { { ir_size::bit_32 }, "32" },
-            { { ir_size::bit_64 }, "64" },
+            { { ir_size::bit_8 }, "dec 8" },
+            { { ir_size::bit_16 }, "dec 16" },
+            { { ir_size::bit_32 }, "dec 32" },
+            { { ir_size::bit_64 }, "dec 64" },
         };
     }
 
@@ -41,10 +41,28 @@ namespace eagle::ir::handler
 
 namespace eagle::ir::lifter
 {
+    bool dec::virtualize_as_address(codec::dec::operand operand, uint8_t idx)
+    {
+        return base_x86_translator::virtualize_as_address(operand, idx);
+    }
+
     void dec::finalize_translate_to_virtual()
     {
         block->add_command(std::make_shared<cmd_rflags_load>());
         base_x86_translator::finalize_translate_to_virtual();
         block->add_command(std::make_shared<cmd_rflags_store>());
+
+        codec::dec::operand first_op = operands[0];
+        if(first_op.type == ZYDIS_OPERAND_TYPE_REGISTER)
+        {
+            // register
+            codec::reg reg = static_cast<codec::reg>(first_op.reg.value);
+            block->add_command(std::make_shared<cmd_context_store>(reg));
+        }
+        else
+        {
+            // memory
+        }
+
     }
 }
