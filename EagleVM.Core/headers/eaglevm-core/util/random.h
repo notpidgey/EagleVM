0cf2679743d684e009bbb828d9120ec719da43a4
Pidgey <notpidgey@users.noreply.github.com>
2024-06-19 03:55:02 -0400

    implemented random in cpp file

--- EagleVM.Core/headers/eaglevm-core/util/random.h
+++ EagleVM.Core/headers/eaglevm-core/util/random.h
@@ -8,56 +8,15 @@ namespace eagle::util
     public:
         std::mt19937 gen{};
 
-        ran_device()
-        {
-            std::random_device rd{};
+        ran_device();
+        static ran_device& get();
 
-#if _DEBUG
-            gen.seed(0xDEADBEEF);
-#else
-            gen.seed(0xDEADBEEF);
-#endif
-        }
-
-        static ran_device& get()
-        {
-            static ran_device instance;
-            return instance;
-        }
-
-        uint64_t gen_64()
-        {
-            std::uniform_int_distribution<std::mt19937::result_type> dist(0, UINT64_MAX);
-            return dist(gen);
-        }
-
-        uint32_t gen_32()
-        {
-            std::uniform_int_distribution<std::mt19937::result_type> dist(0, UINT32_MAX);
-            return dist(gen);
-        }
-
-        uint16_t gen_16()
-        {
-            std::uniform_int_distribution<std::mt19937::result_type> dist(0, UINT16_MAX);
-            return dist(gen);
-        }
-
-        uint8_t gen_8()
-        {
-            std::uniform_int_distribution<std::mt19937::result_type> dist(0, UINT8_MAX);
-            return dist(gen);
-        }
-
-        uint64_t gen_dist(std::uniform_int_distribution<uint64_t>& distribution)
-        {
-            return distribution(gen);
-        }
-
-        double gen_dist(std::uniform_real_distribution<>& distribution)
-        {
-            return distribution(gen);
-        }
+        uint64_t gen_64();
+        uint32_t gen_32();
+        uint16_t gen_16();
+        uint8_t gen_8();
+        uint64_t gen_dist(std::uniform_int_distribution<uint64_t>& distribution);
+        double gen_dist(std::uniform_real_distribution<>& distribution);
 
         ran_device(const ran_device&) = delete;
         ran_device& operator=(const ran_device&) = delete;
